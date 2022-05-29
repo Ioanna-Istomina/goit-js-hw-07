659159c0da3738eae958a86bcc74fe4429398c6b
Ioanna-Istomina <99349464+Ioanna-Istomina@users.noreply.github.com>
2022-05-29 14:36:57 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+//Додавання розмітки до HTML
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+//Додавання слухача на галерею. Делегування на div.gallery
+gallery.addEventListener("click", onGalleryItemClick);
+
+//Створення розмітки галереї
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+function onGalleryItemClick(ev) {
+  //Прибирає перезавантаження сторінки після кліку
+  ev.preventDefault();
+
+  if (ev.target.nodeName !== "IMG") {
+    return;
+  }
+  //Отримує URL великої картинки
+  const url = ev.target.dataset.source;
+  const alt = ev.target.alt;
+}
